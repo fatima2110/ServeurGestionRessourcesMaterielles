d1c631e720b3dc5ad8b3af49179fb3f380f82530
fatima2110 <fatimaamiri2030@gmail.com>
2023-04-11 20:59:10 +0000

    résolution des conflits

--- src/main/java/ma/fstf/ServeurGestionRessourcesMaterielles/auth/AuthenticationController.java
+++ src/main/java/ma/fstf/ServeurGestionRessourcesMaterielles/auth/AuthenticationController.java
@@ -8,26 +8,25 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
-<<<<<<< HEAD
-@CrossOrigin("*")
-=======
->>>>>>> b8ce556eac4b5e2030eadc704d2fd1d88cf5a3d4
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
   private final AuthenticationService service;
   private final LogoutService logoutService;
 
+  @CrossOrigin(origins="*")
   @PostMapping("/register")
   public ResponseEntity<AuthenticationResponse> register(
-      @RequestBody RegisterRequest request
+          @RequestBody RegisterRequest request
   ) {
     return ResponseEntity.ok(service.register(request));
   }
+  @CrossOrigin(origins="*")
   @PostMapping("/authenticate")
   public ResponseEntity<AuthenticationResponse> authenticate(
-      @RequestBody AuthenticationRequest request
+          @RequestBody AuthenticationRequest request
   ) {
     return ResponseEntity.ok(service.authenticate(request));
   }
@@ -37,9 +36,6 @@ public class AuthenticationController {
     logoutService.logout(request,null,null);
     return new ResponseEntity<>("déconnexion réussie", HttpStatus.OK);
   }
-<<<<<<< HEAD
-=======
 
 
->>>>>>> b8ce556eac4b5e2030eadc704d2fd1d88cf5a3d4
 }
